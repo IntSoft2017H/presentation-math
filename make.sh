14dcc8bb52c1e6d9a07c7710af7a68bd9f224ca8
nekketsuuu <nekketsuuu@users.noreply.github.com>
2017-12-14 21:50:06 +0900

    Add a script for making a SVG file from TeX sources

---
+++ make.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -eu
+
+if [ $# -lt 1 ]; then
+    echo "Usage:" ;
+    echo "    $0 <image name>" ;
+    echo "Example:" ;
+    echo "    $0 gamma" ;
+    echo "Note:" ;
+    echo "    You should install latexmk and dvisvgm, and config latexmk properly before using this script."
+    exit 1
+fi
+
+set -x
+
+cd "$1"
+TEXINPUTS=".:..:$TEXINPUTS" latexmk
+dvisvgm "$1.dvi"
